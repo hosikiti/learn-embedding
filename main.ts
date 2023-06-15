548d7e308b653d1f9ff9727bf0643edb6e3abc8d
Kei Oikawa <keio@Kei-Mac.local>
2023-06-15 20:35:16 +0900

    initial commit.

--- main.ts
+++ main.ts
@@ -1,11 +1,34 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Configuration, CreateEmbeddingResponse, OpenAIApi } from "npm:openai";
+import * as similarity from "https://esm.sh/compute-cosine-similarity";
 
-const app = new Application();
-
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+const configuration = new Configuration({
+  apiKey: Deno.env.get("API_KEY"),
 });
+const openai = new OpenAIApi(configuration);
 
-const serverPort = parseInt(Deno.env.get("DENO_SERVER_PORT") || "3003");
-console.log("http server started on:", serverPort);
-await app.listen({ port: serverPort });
+async function getEmbedding(input: string): Promise<number[]> {
+  const resp = await openai.createEmbedding({
+    input: input,
+    model: "text-embedding-ada-002",
+  });
+
+  return resp.data.data[0].embedding;
+}
+
+async function main() {
+  const vec1 = await getEmbedding(
+    "カレンダーから洋服の登録ができない。服の読み込み画面で登録した洋服が読み込み中のまま更新されない",
+  );
+  const vec2 = await getEmbedding(
+    "何度もエラーになりましたのメッセージが出て自分のカレンダーコーデの登録や回答の記入ができないことがあります\n問合せもなかなかできなかったです",
+  );
+  const vec3 = await getEmbedding(
+    "家族分も別のクローゼットで管理できるようにしてほしい",
+  );
+
+  console.log("Vec1 - Vec2 Similarity -> ", similarity.default(vec1, vec2));
+  console.log("Vec1 - Vec3 Similarity -> ", similarity.default(vec1, vec3));
+  console.log("Vec2 - Vec3 Similarity -> ", similarity.default(vec2, vec3));
+}
+
+main();
